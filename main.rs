72b21642422662385a895aa50b2a4eee63460c2d
farhangold <121116261+farhangold@users.noreply.github.com>
2024-04-20 20:49:02 +0700

    tan number function

--- main.rs
+++ main.rs
@@ -16,6 +16,9 @@ fn modulo_numbers(num1: i32, num2: i32) -> i32 {
 fn sin(angle: f64) -> f64 {
     angle.sin()
 }
+fn tan(angle: f64) -> f64 {
+    angle.tan()
+}
 fn main() {
     println!("Hello World");
 }
