ea6b34e02d345b41f4e3cfcc5bf83f56b09ae7e6
Ryunehaimu <ryunehaimu007@gmail.com>
2024-04-20 20:30:00 +0700

    add devide_numbers function

--- main.rs
+++ main.rs
@@ -4,6 +4,9 @@ fn add_numbers(num1: i32, num2: i32) -> i32 {
 fn subtract_numbers(num1: i32, num2: i32) -> i32 {
     num1 - num2
 }
+fn devide_numbers(num1: i32, num2: i32) -> i32 {
+    num1 / num2
+}
 fn main() {
     println!("Hello World");
 }
